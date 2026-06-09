16f05c7fd82880d94aeebff7ac56777c9ca7f274
xtekkis <211207676+xtekkis@users.noreply.github.com>
2026-06-09 21:01:06 +0300

    Added player car with keyboard movement and WASD support

--- client/game.js
+++ client/game.js
@@ -3,6 +3,12 @@ const config = {
   width: 1280,
   height: 720,
   backgroundColor: '#1a1a2e',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false
+    }
+  },
   scene: {
     preload: preload,
     create: create,
@@ -12,10 +18,61 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-function preload() {}
+let player;
+let cursors;
+
+function preload() { }
 
 function create() {
   this.track = new Track(this);
+
+  player = this.physics.add.image(175, 360, null);
+  player = this.add.circle(175, 360, 12, 0xe8c14a);
+  player.setDepth(1);
+
+  this.playerBody = this.physics.add.existing(
+    this.add.rectangle(175, 360, 24, 24, 0x000000, 0)
+  );
+  this.playerBody.body.setCollideWorldBounds(true);
+  this.playerBody.body.setMaxVelocity(200, 200);
+
+  cursors = this.input.keyboard.createCursorKeys();
+  this.wasd = {
+    up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+    down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+    left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+    right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+  };
+
+  this.playerAngle = 0;
+  this.playerSpeed = 0;
 }
 
-function update() {}
+function update() {
+  const speed = 180;
+  const turnSpeed = 3;
+
+  if (cursors.left.isDown || this.wasd.left.isDown) {
+    this.playerAngle -= turnSpeed;
+  } else if (cursors.right.isDown || this.wasd.right.isDown) {
+    this.playerAngle += turnSpeed;
+  }
+
+  if (cursors.up.isDown || this.wasd.up.isDown) {
+    this.playerSpeed = speed;
+  } else if (cursors.down.isDown || this.wasd.down.isDown) {
+    this.playerSpeed = -speed / 2;
+  } else {
+    this.playerSpeed *= 0.95;
+  }
+
+  const rad = Phaser.Math.DegToRad(this.playerAngle);
+  const vx = Math.cos(rad) * this.playerSpeed;
+  const vy = Math.sin(rad) * this.playerSpeed;
+
+  this.playerBody.x += vx * 0.016;
+  this.playerBody.y += vy * 0.016;
+
+  player.x = this.playerBody.x;
+  player.y = this.playerBody.y;
+}
